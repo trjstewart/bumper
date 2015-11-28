26e344dbb5cffc01ffbf66f1bddd27ec394a448d
David <david.buchanswanson@gmail.com>
2015-11-28 19:37:29 +1000

    unchange port

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ app.post('/report', function (req, res) {
 
 });
 
-var server = app.listen(80, function () {
+var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
