3fd660e2e262973ea9af7da993af39c2284a944d
David <david.buchanswanson@gmail.com>
2015-11-28 19:32:40 +1000

    change port to 80

--- server.js
+++ server.js
@@ -71,10 +71,10 @@ app.get('/stilist', function (req, res) {
 });
 
 app.post('/report', function (req, res) {
-	
+
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(80, function () {
   var host = server.address().address;
   var port = server.address().port;
 
