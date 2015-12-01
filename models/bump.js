33eb2269b7abc6c0a6c006e5f0d9aae819b2f79e
David <david.buchanswanson@gmail.com>
2015-12-02 07:46:34 +1000

    fix some chars

--- models/bump.js
+++ models/bump.js
@@ -1,13 +1,13 @@
 var mongoose = require('mongoose')
-      , Schema = mongoose.Schema
+      , Schema = mongoose.Schema;
       bumpSchema = new Schema( {
           user1: String,
           user2: String,
           distance: Number,
           centerX: Number,
           centerY: Number,
-          dateTime: { type : Date, default: Date.now },
-      }),
+          dateTime: { type : Date, default: Date.now }
+      });
 Bump = mongoose.model('bump', bumpSchema);
 
 module.exports = Bump;
